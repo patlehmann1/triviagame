fd48580b64551f485c83dd5491398f87bfcf68f6
Patrick Lehmann <patlehmann1@gmail.com>
2018-05-29 22:34:59 -0400

    just need to add comparison logic, really

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,11 @@
 var timeRemaining = 120;
 var answersRight;
 var answersWrong;
+var intervalId;
 
 function pageLoad() {
     $("#full_game").hide();
-    $("#total_score").hide();
+    $(".total_score").hide();
     $("#start_button").on("click", gameStart);
     $("#time_remaining").html("Time Remaining: "+ timeRemaining + " seconds");
 }
@@ -12,7 +13,27 @@ function pageLoad() {
 function gameStart(){
     $("#full_game").show();
     $("#start_button").hide();
+    run();
 }
+
+function run(){
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    timeRemaining--;
+    $("#time_remaining").html("Time Remaining: "+ timeRemaining + " seconds");
+    if (timeRemaining === 0) {
+      stop();
+      $("#full_game").hide();
+      $(".total_score").show();  
+    }
+  }
+
+  function stop() {
+    clearInterval(intervalId);
+  }
 
 
 
