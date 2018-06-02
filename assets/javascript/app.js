1fae135949ff533f394ed73b19e64f24888c1d04
Patrick Lehmann <patlehmann1@gmail.com>
2018-06-02 00:07:02 -0400

    game functions for now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,21 @@
-var timeRemaining = 120;
+var secondsRemaining= 60;
 var answersRight = 0;
 var answersWrong = 0;
+var unanswered = 0;
 var intervalId;
 
 function pageLoad() {
     $("#full_game").hide();
+    $("#restart_button").hide();
     $("#start_button").on("click", gameStart);
-    $("#time_remaining").html("Time Remaining: "+ timeRemaining + " seconds");
-    
+   
 }
 
 function gameStart(){
     $("#full_game").show();
     $("#start_button").hide();
+    $("#time_remaining").html("Time Remaining: "+ secondsRemaining + " seconds");   
     run();
-    checkAnswers();
 }
 
 function run(){
@@ -23,9 +24,10 @@ function run(){
 }
 
 function decrement() {
-    timeRemaining--;
-    $("#time_remaining").html("Time Remaining: "+ timeRemaining + " seconds");
-    if (timeRemaining === 0) {
+    secondsRemaining--;
+    $("#time_remaining").html("Time Remaining: "+ secondsRemaining + " seconds");   
+    
+    if (secondsRemaining === 0) {
       stop();
       endGame();
     }
@@ -36,21 +38,46 @@ function decrement() {
   }
 
   function endGame(){
+
     $("#full_game").hide();
-    $(".total_score").show(); 
-  }
+    $(".total_score").show();
 
-  function checkAnswers(){
+    var Q1 = $('input:radio[name="q1"]:checked').val();
+		var Q2 = $('input:radio[name="q2"]:checked').val();
+		var Q3 = $('input:radio[name="q3"]:checked').val();
+		var Q4 = $('input:radio[name="q4"]:checked').val();
+		var Q5 = $('input:radio[name="q5"]:checked').val();
+    var Q6 = $('input:radio[name="q6"]:checked').val();
+    var Q7 = $('input:radio[name="q7"]:checked').val();
 
-    $('.rightAns').click(function() {
+    function checkAnswers(questionName, answer){
+      if(questionName == undefined){
+        unanswered++;
+      }
+      else if(questionName == answer){
         answersRight++;
-        $("#guessedRight").html("Correct answers: " + answersRight);
-     });
+      }
+      else{
+        answersWrong++;
+      }
+  
+    }
 
-    $('.wrongAns').click(function() {
-    answersWrong++;
-    $("#guessedWrong").html("Incorrect answers: " + answersWrong);
-    });
+    checkAnswers(Q1, "1930");
+    checkAnswers(Q2, "Gary Lineker");
+    checkAnswers(Q3, "Germany");
+    checkAnswers(Q4, "Andres Escobar");
+    checkAnswers(Q5, "False");
+    checkAnswers(Q6, "6");
+    checkAnswers(Q7, "Brazil");
+    
+
+    $("#guessedRight").html("Right Guesses: " + answersRight);
+    $("#guessedWrong").html("Wrong Guesses: " + answersWrong);
+    $("#unanswered").html("Unanswered: " + unanswered);
+    $("#restart_button").show();
+
+    
   }
 
 Document.onload = pageLoad();
